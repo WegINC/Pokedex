e0acae96a1fd3ca9e0e5f298b97e9dd2167a85c4
WegINC <wegmeyert@gmail.com>
2025-04-18 17:55:25 +0200

    first commit

---
+++ scripts/loading.js
@@ -0,0 +1,75 @@
+function showLoadingSpinner(){
+    let loadBtnRef = document.getElementById("loadBtn");
+    loadBtnRef.classList.add("d-none");
+    let loadingRef = document.getElementById("loading");
+    loadingRef.innerHTML = templateLoading();
+  }
+  
+  function disableLoadingSpinner(){
+    let loadingRef = document.getElementById("loading");
+    loadingRef.innerHTML = "";
+    loadingRef.classList.remove("loading");
+    let contentRef = document.getElementById("content");
+    contentRef.classList.toggle("d-none");
+    let loadBtnRef = document.getElementById("loadBtn");
+    loadBtnRef.classList.remove("d-none");
+  }
+
+  function loadingPokemon(){
+    let contentRef = document.getElementById("content");
+    contentRef.classList.add("d-none");
+    let loadBtnRef = document.getElementById("loadBtn");
+    loadBtnRef.classList.add("d-none");
+    let loadingRef = document.getElementById("loading");
+    loadingRef.classList.add("loading");
+    loadingRef.innerHTML = templateLoading();
+  }
+
+  function disableLoadingPokemon(){
+    let contentRef = document.getElementById("content");
+    contentRef.classList.remove("d-none");
+    let loadBtnRef = document.getElementById("loadBtn");
+    loadBtnRef.classList.remove("d-none");
+    let loadingRef = document.getElementById("loading");
+    loadingRef.classList.remove("loading");
+    loadingRef.innerHTML = "";
+  }
+
+  function goDown(){
+    let contentRef = document.getElementById("content");
+    window.scrollTo({
+      top: contentRef.scrollHeight
+    })
+  }
+
+  function loadingContent(){
+    let bodyRef = document.getElementById("body");
+    bodyRef.classList.add("overflowHidden");
+    let loadingRef = document.getElementById("loading");
+    loadingRef.classList.add("overlay");
+    loadingRef.innerHTML = templateLoading();
+  }
+
+  function disableLoadingContent(){
+    let loadingRef = document.getElementById("loading");
+    loadingRef.innerHTML="";
+    loadingRef.classList.remove("overlay");
+    let contentOverlayRef = document.getElementById("overlay");
+    contentOverlayRef.classList.remove("d-none");
+  }
+
+  function loadMoreAboutPokemon(){
+    let moreAboutPokemonRef = document.getElementById("moreAboutPokemon");
+    moreAboutPokemonRef.classList.add("d-none");
+    let loadMoreAboutPokemonRef = document.getElementById("loadMoreAboutPokemon");
+    loadMoreAboutPokemonRef.innerHTML = templateLoading();
+    loadMoreAboutPokemonRef.classList.remove("d-none");
+  }
+
+  function disableLoadMoreAboutPokemon(){
+    let moreAboutPokemonRef = document.getElementById("moreAboutPokemon");
+    moreAboutPokemonRef.classList.remove("d-none");
+    let loadMoreAboutPokemonRef = document.getElementById("loadMoreAboutPokemon");
+    loadMoreAboutPokemonRef.innerHTML="";
+    loadMoreAboutPokemonRef.classList.add("d-none");
+  }
